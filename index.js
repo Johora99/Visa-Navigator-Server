67342a9858cae8b80c739eb7479394d8ace4abd6
Johora99 <fatimatuzjohora1999@gmail.com>
2024-12-05 23:06:04 +0600

    single visa get

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-
-// visaNavigator
-// icIzEBjOaEXFPpQG
-
 app.get('/',(req,res)=>{
   res.send('Hello')
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3oeok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,7 +27,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-  
+    const allVisaCollection = client.db('userCollection').collection('visaApplication');
+    app.get('/visaApplication/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await allVisaCollection.findOne(query);
+      res.send(result)
+    })
+    app.get('/visaApplication',async(req,res)=>{
+        const cursor = allVisaCollection.find()
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+    app.get('/newVisas',async(req,res)=>{
+    const cursor = allVisaCollection.find().sort({ _id: -1 }).limit(8);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    app.post('/visaApplication',async(req,res)=>{
+      const newVisa = req.body;
+      const result = await allVisaCollection.insertOne(newVisa);
+      res.send(result);
+    })
   
   } finally {
     // Ensures that the client will close when you finish/error
