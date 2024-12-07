a83c5e7ea4b114cced4b69c96ce40080b33a7d1f
Johora99 <fatimatuzjohora1999@gmail.com>
2024-12-07 23:12:46 +0600

    all data seen

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ async function run() {
       const query = {_id : new ObjectId(id)};
       const result = await myVisa.deleteOne(query);
       res.send(result)
-      console.log(result)
+      
      })
 
 
