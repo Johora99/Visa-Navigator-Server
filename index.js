27e9da20045d1b919b409578d6da80ab8c18e775
Johora99 <fatimatuzjohora1999@gmail.com>
2024-12-07 23:12:07 +0600

    result seen

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/',(req,res)=>{
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3oeok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -146,8 +146,8 @@ async function run() {
       const query = {_id : new ObjectId(id)};
       const result = await myVisa.deleteOne(query);
       res.send(result)
+      console.log(result)
      })
-
 
 
   } finally {
